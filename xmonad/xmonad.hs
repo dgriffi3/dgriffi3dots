0cc3d726d180f972d41e970bc987f22f905ce8f5
Dennis Griffith <dgriffi3@uiuc.edu>
2010-05-19 15:49:44 -0500

    remove unused monad stuff

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -26,19 +26,7 @@ import XMonad.Prompt.Window
 
 import System.IO(hPutStrLn)
 
-myLayoutHook = Full ||| tiled ||| Mirror tiled ||| Grid
-  where
-    -- default tiling algorithm partitions the screen into two panes
-    tiled   = Tall nmaster delta ratio
-              
-    -- The default number of windows in the master pane
-    nmaster = 1
-
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1/2
-
-    -- Percent of screen to increment by when resizing panes
-    delta   = 3/100
+myLayoutHook = Full
 
 -- MyFloatsC and T are match-anywher
 myManageHook = composeAll . concat $
